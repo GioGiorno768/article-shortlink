4f5fd082fd7440e0fdb9e9e03e5899b1178748cc
GioGiorno <kevinragill768@gmail.com>
2026-01-29 18:10:40 +0700

    add monetag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
   other: {
     "70b3de18186dd048db2aaa806c225c9907c188ab":
       "70b3de18186dd048db2aaa806c225c9907c188ab",
+    monetag: "0be7013df1b872d24e3f8dbbba1f8b86",
   },
 };
 
